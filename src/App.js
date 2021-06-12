63c40049e1585e5f6b8fc9d2fe52c22e5a8219f2
Mahantesh.Patil <mahantesh.patil@Mahanteshs-MacBook-Pro.local>
2021-06-12 06:50:31 +0530

    testing the API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function App() {
+  const getData = async () => {
+    try {
+      const data = await axios.get(
+        'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
+      );
+      console.log(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
       Hello world
