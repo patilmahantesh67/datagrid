17171e1be1346ec7d44a091d094833fdfafd1e6c
Mahantesh.Patil <mahantesh.patil@Mahanteshs-MacBook-Pro.local>
2021-06-14 16:29:46 +0530

    checkbox implemented

--- src/datatable/index.jsx
+++ src/datatable/index.jsx
@@ -6,11 +6,30 @@ export default function DataTable(props) {
 		const filteredPeople = props.data.filter((item) => item.id !== idToRemove);
 		props.onChange(filteredPeople);
 	}
+	const checkBoxUpdate = (isChecked, value) => {
+		for (var i in props.value) {
+			if (props.value[i].id === value) {
+				props.value[i].checkbox = isChecked;
+				 break; //Stop this loop, we found it!
+			}
+		}
+	}
+	const removeSelectedRows = () => {
+		const hello = props.data;
+		let filteredArray = hello.filter(value => value.checkbox === false);
+		props.onChange(filteredArray);
+	}
 	const renderBody = () => {
 		return props.value && props.value.map(({ id, name, email, role }) => {
 			return (
 				<tr key={id}>
-					<td>{id}</td>
+					<td>
+						<input 
+							type="checkbox"
+							value={id}
+							onChange={(event) => checkBoxUpdate(event.target.checked, event.target.value)}
+						/>
+					</td>
 					<td>{name}</td>
 					<td>{email}</td>
 					<td>{role}</td>
@@ -21,12 +40,19 @@ export default function DataTable(props) {
 			)
 		})
 	}
-	return <table cellPadding={0} cellSpacing={0}>
-		<thead>
-			<tr>{props.value[0] && columns.map((heading) => <th>{heading}</th>)}</tr>
-		</thead>
-		<tbody>
-				{renderBody()}
-		</tbody>
-	</table>
+	return (
+	<>
+		<table cellPadding={0} cellSpacing={0}>
+			<thead>
+				<tr>{props.value[0] && columns.map((heading) => <th>{heading}</th>)}</tr>
+			</thead>
+			<tbody>
+					{renderBody()}
+			</tbody>
+		</table>
+		<div>
+			<button onClick={removeSelectedRows}>Delete Selected</button>
+		</div>
+	</>
+	)
 }
