72f71eac990995f7cbc1b5c83eb37c3953076996
Mahantesh.Patil <mahantesh.patil@Mahanteshs-MacBook-Pro.local>
2021-06-20 21:50:28 +0530

    bug of individual select is resolved

--- src/datatable/index.jsx
+++ src/datatable/index.jsx
@@ -1,27 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function DataTable(props) {
 	const [allChecked, setAllChecked] = useState(false);
+	const [checkedArray, setCheckedArray] = useState([]);
 	// const columns = props.gridData[0] && Object.keys(props.gridData[0]);
 	const columns = ['name', 'email', 'role', 'actions'];
+	useEffect(() => {
+		const initialIsChecked = props.totalData.reduce((acc, d) => {
+			acc[d.id] = false;
+			return acc;
+		}, {});
+		setCheckedArray(initialIsChecked);
+	}, [props]);
 	const removeData = (idToRemove) => {
 		const filteredPeople = props.totalData.filter((item) => item.id !== idToRemove);
 		props.onChange(filteredPeople);
 	}
-	const updateCheckbox = (isChecked, value) => {
+	const updateCheckbox = (event) => {
 		for (var i in props.gridData) {
-			if (props.gridData[i].id === value) {
-				props.gridData[i].checkbox = isChecked;
+			if (props.gridData[i].id === event.target.value) {
+				props.gridData[i].checkbox = event.target.checked;
 				break; //Stop this loop, we found it!
 			}
 		}
+		setCheckedArray({ ...checkedArray, [parseInt(event.target.value)]: event.target.checked });
 	}
 	const updateAllCheckBoxes = (e) => {
 		const pageData = props.gridData;
+		const tempCheckboxArray = [];
 		pageData.forEach(element => {
 			element.checkbox = e.target.checked;
+			tempCheckboxArray[parseInt(element.id)] = e.target.checked;
 		});
 		setAllChecked(e.target.checked);
+		setCheckedArray({ ...checkedArray, ...tempCheckboxArray});
 	}
 	const removeSelectedRows = () => {
 		let filteredArray = props.totalData.filter(value => value.checkbox === false);
@@ -36,8 +48,8 @@ export default function DataTable(props) {
 						<input 
 							type="checkbox"
 							value={id}
-							defaultChecked={checkbox}
-							onChange={(event) => updateCheckbox(event.target.checked, event.target.value)}
+							checked = {checkedArray ? checkedArray[id] : false}
+							onChange={updateCheckbox}
 						/>
 					</td>
 					<td>{name}</td>
